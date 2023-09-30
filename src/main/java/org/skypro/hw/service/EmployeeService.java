f07134702b3568eb5c4d981b22f52d5be46288e8
maks <maxim_86nva@mail.ru>
2023-09-30 13:00:49 +0300

    валидация/исправление данных с помощью apache common

--- src/main/java/org/skypro/hw/service/EmployeeService.java
+++ src/main/java/org/skypro/hw/service/EmployeeService.java
@@ -1,9 +1,11 @@
 package org.skypro.hw.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.skypro.hw.entity.Employee;
 import org.skypro.hw.exception.EmployeeAlreadyAddedException;
 import org.skypro.hw.exception.EmployeeNotFoundException;
 import org.skypro.hw.exception.EmployeeStorageIsFullException;
+import org.skypro.hw.exception.ValidationException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,6 +19,13 @@ public class EmployeeService {
     private final static int MAX_SIZE = 2;
 
     public Employee add(String firstName, String lastName, double salary, int departmentId) {
+
+        firstName = StringUtils.capitalize(firstName);
+        lastName = StringUtils.capitalize(lastName);
+
+
+
+        validateFirstAndLastNames(firstName, lastName);
 
         if (employees.size() >= MAX_SIZE) {
             throw new EmployeeStorageIsFullException("Массив сотрудников переполнен");
@@ -32,7 +41,13 @@ public class EmployeeService {
         return newEmployee;
     }
 
+
     public Employee find(String firstName, String lastName, double salary, int departmentId) {
+        firstName = StringUtils.capitalize(firstName);
+        lastName = StringUtils.capitalize(lastName);
+
+        validateFirstAndLastNames(firstName, lastName);
+
         Employee employeeForFound = new Employee(firstName, lastName, salary, departmentId);
         for (Employee e : employees) {
             if (e.equals(employeeForFound)) {
@@ -44,6 +59,10 @@ public class EmployeeService {
     }
 
     public Employee remove(String firstName, String lastName, double salary, int departmentId) {
+        firstName = StringUtils.capitalize(firstName);
+        lastName = StringUtils.capitalize(lastName);
+
+        validateFirstAndLastNames(firstName, lastName);
         Employee employeeForRemove = new Employee(firstName, lastName, salary, departmentId);
 
         boolean removeResult = employees.remove(employeeForRemove);
@@ -56,5 +75,15 @@ public class EmployeeService {
 
     public List<Employee> getAll() {
         return employees;
+    }
+
+    private void validateFirstAndLastNames(String firstName, String lastName) {
+        if (!StringUtils.isAlpha(firstName)) {
+            throw new ValidationException("Имя содержит запрещенные символы");
+        }
+
+        if (!StringUtils.isAlpha(lastName)) {
+            throw new ValidationException("Фамилия содержит запрещенные символы");
+        }
     }
 }
